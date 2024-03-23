ede4fe28a998e039a24152c89242d8df4aaa920f
Dharamveer Singh <96969894+dveersingh000@users.noreply.github.com>
2024-03-23 23:46:59 +0530

    Solution added

---
+++ LeetCode/March/23-03-2024/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void reorderList(ListNode head) {
+       
+        ListNode fast = head, slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+       
+        ListNode cur = slow.next;
+        slow.next = null;
+
+       
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode t = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = t;
+        }
+        cur = head;
+
+        
+       
+        while (pre != null) {
+            ListNode t = pre.next;
+            pre.next = cur.next;
+            cur.next = pre;
+            cur = pre.next;
+            pre = t;
+        }
+    }
+}
