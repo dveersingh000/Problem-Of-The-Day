ffd49065f121c7ccd8b4be2c1d5b2eee1a5a7d80
Dharamveer Singh <96969894+dveersingh000@users.noreply.github.com>
2024-08-24 22:12:41 +0530

    Create GFG.java

---
+++ GeeksForGeeks/Augest/24-08-2024/GFG.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[]) {
+        // your code here
+        int n = val.length;
+        int[][] dp = new int[n + 1][W + 1];
+        
+        // Building the DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= W; j++) {
+                if (wt[i - 1] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], val[i - 1] + dp[i - 1][j - wt[i - 1]]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[n][W];
+    }
+}
