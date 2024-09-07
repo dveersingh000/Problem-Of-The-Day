21400df8314c5b10d73a68ae4389bd0084eb0a0f
Dharamveer Singh <96969894+dveersingh000@users.noreply.github.com>
2024-09-07 23:20:57 +0530

    Create GFG.java

---
+++ GeeksForGeeks/September/07-09-2024/GFG.java
@@ -0,0 +1,14 @@
+class Solution {
+    long findNth(long n) {
+        long result = 0;
+        long base = 1;
+        
+        while (n > 0) {
+            result += (n % 9) * base;
+            n /= 9;
+            base *= 10;
+        }
+        
+        return result;
+    }
+}
