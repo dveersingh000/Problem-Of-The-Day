9a2c6f7eed7633b93667e308663179c3d522d335
Dharamveer Singh <96969894+dveersingh000@users.noreply.github.com>
2024-09-09 23:51:26 +0530

    Create Leet.java

---
+++ LeetCode/September/09-09-2024/Leet.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int[][] ans = new int[m][n];
+        for (var row : ans) {
+            Arrays.fill(row, -1);
+        }
+        int i = 0, j = 0, k = 0;
+        final int[] dirs = {0, 1, 0, -1, 0};
+        while (true) {
+            ans[i][j] = head.val;
+            head = head.next;
+            if (head == null) {
+                break;
+            }
+            while (true) {
+                int x = i + dirs[k], y = j + dirs[k + 1];
+                if (x >= 0 && x < m && y >= 0 && y < n && ans[x][y] == -1) {
+                    i = x;
+                    j = y;
+                    break;
+                }
+                k = (k + 1) % 4;
+            }
+        }
+        return ans;
+    }
+}
